caee46bf31eb647d3daede9ea3af2dd094181d42
ehabtalaat <ehabtalaat5552@gmail.com>
2022-02-16 14:32:19 +0200

    add

---
+++ Envoy.blade.php
@@ -0,0 +1,23 @@
+@servers(['web' =>'azcour@azcourses.net'])
+
+
+@setup
+    $repository = 'git@github.com:AhmedNader65/Coursat.git';
+    $app_dir = '/home/azcour/public_html/';
+@endsetup
+
+@story('deploy')
+    pull_repository
+@endstory
+
+
+@task('pull_repository')
+    echo 'Pulling repository'
+    cd {{ $app_dir }}
+    git pull origin master
+
+
+
+@endtask
+
+...
