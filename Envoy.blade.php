5b6cca3e882e06e2e1db267db602504ce6b9eda6
ehabtalaat <ehabtalaat5552@gmail.com>
2022-02-16 16:31:13 +0200

    add

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -14,8 +14,8 @@
 @task('pull_repository')
     echo 'Pulling repository'
     cd {{ $app_dir }}
-    <!-- git add . 
-    git commit -m "add" -->
+    git add . 
+    git commit -m "add"
     git stash
     git pull origin master
 
