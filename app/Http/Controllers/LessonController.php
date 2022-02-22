51238ff37a1b19b920cad98f95c11a1c7065871f
ehabtalaat <ehabtalaat5552@gmail.com>
2022-02-22 12:51:58 +0200

    Add

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -426,6 +426,41 @@ if($request->pay){
            $video->user_id = auth()->user()->id;
        }
        $video->save();
+       $students = $lesson->typescollege->studentscollege; 
+       foreach($students as $user){
+         $not = new Notification;
+         $text = 'لديك حصه جديده فى كورس ' . $lesson->typescollege->name_ar;
+     $not->title = 'اشعار جديد';
+      $not->text = $text;
+     $not->user_id = $user->id;
+     $not->save();
+     $to = $user->device_token;
+          $data = [
+             "to" =>$to,
+             'notification'=>[
+                 'title' => $request->title,
+                 'body' => $request->text
+                 ],
+             "data" =>[
+                'title' => $request->title,
+                 'body' => $request->text,
+                 "click_action" => "FLUTTER_NOTIFICATION_CLICK",
+                 'type' => 'general'
+                 ], 
+         ];
+      $dataString = json_encode($data);
+         $headers = [
+             'Authorization: key=AAAANEwk9ss:APA91bEuBLaq1kPuYH94OKvkO4EU_-VMrtmnj63KDB-yioNibhvl7bKbJBEQy6IvnuLyMT6AhoBi83vYe5Ds4-UaEzIyZrL9WO7ObUfTk8dIXFMih3upFFjLvPECl2ApuHe_LL2TKu6g',
+             'Content-Type: application/json',
+         ];
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+         curl_setopt($ch, CURLOPT_POST, true);
+         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+       $result=curl_exec($ch);
+             }
              }
             return response()->json(['success' => 'video uploaded','id' => $lesson->typescollege_id]);   }else{
 $msg = 'لقد استهلكت 100% ';
@@ -556,6 +591,41 @@ if($request->pay){
            $video->user_id = auth()->user()->id;
        }
        $video->save();
+       $students = $lesson->typescollege->studentscollege; 
+       foreach($students as $user){
+         $not = new Notification;
+         $text = 'لديك حصه جديده فى كورس ' . $lesson->typescollege->name_ar;
+     $not->title = 'اشعار جديد';
+      $not->text = $text;
+     $not->user_id = $user->id;
+     $not->save();
+     $to = $user->device_token;
+          $data = [
+             "to" =>$to,
+             'notification'=>[
+                 'title' => $request->title,
+                 'body' => $request->text
+                 ],
+             "data" =>[
+                'title' => $request->title,
+                 'body' => $request->text,
+                 "click_action" => "FLUTTER_NOTIFICATION_CLICK",
+                 'type' => 'general'
+                 ], 
+         ];
+      $dataString = json_encode($data);
+         $headers = [
+             'Authorization: key=AAAANEwk9ss:APA91bEuBLaq1kPuYH94OKvkO4EU_-VMrtmnj63KDB-yioNibhvl7bKbJBEQy6IvnuLyMT6AhoBi83vYe5Ds4-UaEzIyZrL9WO7ObUfTk8dIXFMih3upFFjLvPECl2ApuHe_LL2TKu6g',
+             'Content-Type: application/json',
+         ];
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+         curl_setopt($ch, CURLOPT_POST, true);
+         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+       $result=curl_exec($ch);
+             }
              }
             return response()->json(['success' => 'video uploaded','id' => $lesson->typescollege_id]);   }else{
 $msg = 'لقد استهلكت 100% ';
