9bbdf8e5666ddb06471baeff88e5d26697ef75f6
ehabtalaat <ehabtalaat5552@gmail.com>
2022-09-21 15:42:09 +0200

    push status

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -921,6 +921,7 @@ class AuthController extends Controller
                  ]            
                              ]);
    }public function app_status(){
-       return response()->json(["status" => true,"old_app_status" => 0,"new_app_status" => 1]);
+       return response()->json(["status" => true,"old_app_status" => 0,
+       "new_app_status" => 1]);
    }
 }
