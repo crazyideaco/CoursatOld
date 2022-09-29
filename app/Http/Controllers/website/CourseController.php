4e2ee51e86f879acc818763306a5e4af2d7cfc09
ehabtalaat <ehabtalaat5552@gmail.com>
2022-09-29 11:06:20 +0200

    courses

--- app/Http/Controllers/website/CourseController.php
+++ app/Http/Controllers/website/CourseController.php
@@ -7,11 +7,23 @@ use App\Http\Requests\Website\Auth\LoginRequest;
 use App\Http\Controllers\Controller;
 use App\WebsiteStudent;
 use PDO;
+use App\Type;
+use App\Course;
+use App\TypesCollege;
 
 class CourseController extends Controller
 {
         public function courses(){
-            return view('website.courses');
-
+            $user = auth()->guard("website_student")->user();
+            $course_ids = $user->courses->pluck("id")->toArray();
+            if($user->type == 1){
+             $courses = Type::whereIn("id",$course_ids)->get();
+            }elseif($user->type == 2){
+                $courses = TypesCollege::whereIn("id",$course_ids)->get();
+            }
+            elseif($user->type == 3){
+                $courses = Course::whereIn("id",$course_ids)->get();
+            }
+            return view('website.courses',compact("courses"));
         }
 }
