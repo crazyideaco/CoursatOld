ceacbad97ba6cd5d2c9feec6cae400f822697c25
ehabtalaat <ehabtalaat5552@gmail.com>
2022-09-29 11:40:21 +0200

    courses

--- routes/web.php
+++ routes/web.php
@@ -655,7 +655,7 @@ Route::post("filtercourses","FilterCourseController@filtercourses");
     Route::get("login_website","website\LoginController@login")->name("login_website");
     Route::post("signin_website","website\LoginController@signin")->name("signin_website");
     Route::get("logout_website","website\LoginController@logout")->name("logout_website");
-    Route::group(['middleware' => 'website_student'], function() {
+    Route::group(['middleware' => 'website_student:guest'], function() {
   
     Route::get("courses_website","website\CourseController@courses")->name("courses_website");
     });
