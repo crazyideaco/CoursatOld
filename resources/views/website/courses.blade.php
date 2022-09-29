5c5ea0e796f08fa099b86959beb781c87b2fbd89
ehabtalaat <ehabtalaat5552@gmail.com>
2022-09-29 12:46:03 +0200

    courses

--- resources/views/website/courses.blade.php
+++ resources/views/website/courses.blade.php
@@ -7,7 +7,7 @@
           <!-- For loop this card cource -->
           <div class="col-lg-4 col-md-6 col-12">
             <a href="single_course.html" class="card_cource">
-              <img src="{{asset('uploads/'.$course->image}}" alt="cource" />
+              <img src="{{asset('uploads/'.$course->image)}}" alt="cource" />
               <div class="description_card">
                 <h5 class="name">{{$course->name_ar ? ""}}</h5>
                 <h6 class="instractor">{{$course->user_name ?? ""}}</h6>
