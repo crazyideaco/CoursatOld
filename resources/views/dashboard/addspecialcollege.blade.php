3b60cb244acfeb469bb1d80e7b72970293e209a6
ehabtalaat <ehabtalaat5552@gmail.com>
2022-12-14 10:49:41 +0200

    add

--- resources/views/dashboard/addspecialcollege.blade.php
+++ resources/views/dashboard/addspecialcollege.blade.php
@@ -281,7 +281,10 @@ span.btn{
     id="pre${id}">
     <div class="form-group col-5">
                                     <label>اسم الفيديو </label>
-                                   <select name="video_id[]" class="form-control selectpicker"  required  >
+                                   <select name="video_id[]" class="form-control selectpicker"  required 
+                                   style="height: 10px; overflow-y: scroll;"
+        data-live-search="true"
+        data-size="10" >
                                        <option value="0" disabled="disabled" selected="selected"> اختر فيديو</option>
                                     @foreach($videos as $video)
                                      <option value="{{$video->id}}">{{$video->name_ar}} </option>
