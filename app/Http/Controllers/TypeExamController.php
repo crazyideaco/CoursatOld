5718339a94f1debc9edcaf670d739f0147b0803d
ehabtalaat <ehabtalaat5552@gmail.com>
2022-12-14 16:16:37 +0200

    add 14/12/2022

--- app/Http/Controllers/TypeExamController.php
+++ app/Http/Controllers/TypeExamController.php
@@ -156,7 +156,7 @@ class TypeExamController extends Controller
     return view("dashboard.typeexams.edit")->with('id',$id)->with('exam',$exam)->with('questions',$questions)->with('privatequestions',$privatequestions);
   }public function updatetypeexam(Request $request,$id){
    // dd($request->all());
-   dd($request->question_image);
+  
    $exam =  TypeExam::where('id',$id)->first();
       $type = Type::where('id',$exam->type_id)->first();
      $exam->name = $request->name_ar;
