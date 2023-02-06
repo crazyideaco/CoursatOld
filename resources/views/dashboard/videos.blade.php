31d7a7c57e17fa0f8c56fc4d31564ae2c81dda6b
ehabtalaat <ehabtalaat5552@gmail.com>
2023-02-06 15:56:25 +0200

    Add

--- resources/views/dashboard/videos.blade.php
+++ resources/views/dashboard/videos.blade.php
@@ -108,7 +108,7 @@
                                 <div class="col-5">
                                     <button class="btn" style="width:100% !important;">
                                       <a href="{{route('addvideospecial',$id)}}">  <span><i class="fas fa-plus-circle"></i></span>
-                                        + اضافة فيديو من حصص سابقة 
+                                         اضافة فيديو من حصص سابقة 
                                         </a>
                                     </button>
 
