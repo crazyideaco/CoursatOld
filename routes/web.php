1ba7583f53271faef1931327157d65cd458660c8
mostafamhashem <mostafa.m.hashem.1@gmail.com>
2023-11-15 12:22:25 +0200

    test filter

--- routes/web.php
+++ routes/web.php
@@ -251,7 +251,7 @@
 
         Route::get('getSubject_teacher/{id}', 'Dashboard\Filter\TeacherController@getSubject_teacher')->name('getSubject_teacher');
         Route::get('getSubject_teachercollege/{id}', 'Dashboard\Filter\TeacherController@getSubject_teachercollege')->name('getSubject_teachercollege');
-        Route::get('getteacher_type/{id}/{value}', 'Dashboard\Filter\TypeController@gettype')->name('gettype');
+        Route::get('getteacher_type/{id}/{value}', 'Dashboard\Filter\TypeController@gettype')->name('getteacher_type');
 
     });
 
