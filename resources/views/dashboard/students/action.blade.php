e85353354ada17f425a71f18ead2972e28c7f97d
mostafa hashem <mostafa.m.hashem.1@gmail.com>
2023-11-18 21:23:41 +0200

    student dataTAble

---
+++ resources/views/dashboard/students/action.blade.php
@@ -0,0 +1,173 @@
+@php
+    $student = \App\User::find($id);
+@endphp
+<td class="text-center">
+    <span class="btn  btn-sm"style="border:1px solid #222; margin-bottom:10px; padding:6px 45px"
+        onclick="student_logout({{ $id }})">
+        تسجيل الخروج
+    </span>
+    <span class="btn  btn-sm"style="border:1px solid #222; margin-bottom:10px; padding:6px 45px"
+        id="btn{{ $id }}" onclick="activeuser({{ $id }})">
+        @if ($active == 1)
+            الغاء التفعيل
+        @else
+            تفعيل
+        @endif
+    </span>
+    <img src="{{ asset('images/trash.svg') }}" id="trash" onclick="deleteuser('{{ $id }}')"
+        style="cursor:pointer;">
+    @if ($category_id == 1)
+        <a class="btnbtn-sm mt-2"
+            style="border:1px solid #222; margin-bottom:10px; font-size:13px; display:block;    padding: 10px 10px; width: 60%; "
+            href="{{ route('typeresults_students', $id) }}">نتائج
+            الامتحانات</a>
+    @elseif($category_id == 2)
+        <a class="btn btn-sm mt-2" style="border:1px solid #222; margin-bottom:10px; padding:6px 20px"
+            href="{{ route('typecollegeresults_students', $id) }}">نتائج
+            الامتحانات</a>
+    @endif
+    <button type="button" class="btn btn-sm mt-2" style="border:1px solid #222; margin-bottom:10px; padding:6px 20px"
+        data-toggle="modal" data-target="#passwordModal{{ $id }}">
+        ريسيت باسورد الطالب
+    </button>
+
+    <button type="button" class="btn btn-sm mt-2" style="border:1px solid #222; margin-bottom:10px; padding:6px 20px"
+        data-toggle="modal" data-target="#cousesModal{{ $id }}">
+        كورسات الطالب
+    </button>
+</td>
+<!-- The Modal -->
+<div class="modal" id="passwordModal{{ $id }}">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">ريسيت الباسورد للطالب </h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-8">
+                        <label for="password{{ $id }}"> كلمة المرور</label>
+                        <input id="password{{ $id }}" type="password" name="password" class="form-control"
+                            placeholder="كلمة المرور">
+
+                    </div>
+                    <div class="col-8">
+                        <label for="password_confirmation{{ $id }}">تأكيد كلمة المرور</label>
+                        <input id="password_confirmation{{ $id }}"type="password"
+                            name="password_confirmation" class="form-control" placeholder="تاكيد كلمة المرور">
+                    </div>
+                </div>
+                <div class="row  mt-4">
+                    <button type="button text-center" onclick="resetstudentPassword({{ $id }})"
+                        class="btn btn-success">
+                        حفظ
+                    </button>
+                </div>
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<!-- The Modal -->
+<div class="modal" id="cousesModal{{ $id }}">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">كورسات الطالب </h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+                <div class="row">
+                    @if ($student->stutypescollege)
+                        <ul>
+                            @foreach ($student->stutypescollege as $typecollege)
+                                <li style="font-size:14px;">{{ $typecollege->name_ar }}
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
+
+                    @if ($student->stutypes)
+                        <ul>
+                            @foreach ($student->stutypes as $type)
+                                <li style="font-size:14px;">{{ $type->name_ar }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div>
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+{{-- var student_id = $(".student_id:checked").map(function() {
+            return $(this).val();
+        }).get() --}}
+<script>
+    //
+    function resetstudentPassword(id) {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        var table = $('.table').DataTable();
+        $.ajax({
+            type: "post",
+            url: `resetStudentPassword/${id}`,
+            //  contentType: "application/json; charset=utf-8",
+            dataType: "Json",
+            data: {
+                'password': $(`#password${id}`).val(),
+                'password_confirmation': $(`#password_confirmation${id}`).val(),
+            },
+            success: function(result) {
+                if (result.status == true) {
+
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'تم تعديل الباسورد للطلاب بنجاح',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                    table.ajax().reload()
+
+                    //location.reload();
+                } else if (result.status == false) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: result.message,
+                    })
+
+                }
+                $(`#myModal${id}`).modal('hide');
+
+
+
+            }
+
+        });
+    }
+</script>
---
+++ resources/views/dashboard/students/index.blade.php
@@ -0,0 +1,332 @@
+@extends('App.dash')
+@section('style')
+    <style>
+        #example_wrapper {
+            width: 100% !important;
+        }
+    </style>
+@endsection
+@section('content')
+    <!--start page-body-->
+    <div class="page-body">
+        <div class="container">
+            <!--start heed-->
+            <div class="heed">
+
+                <div class="row">
+                    <div class="profile">
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="{{ asset('images/profile.svg') }}">
+                            </div>
+                            <div class="col-6">
+                                <h5>{{ auth()->user()->name }}</h5>
+                                <p>ادمن</p>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                    <div class="flag">
+
+                        <div class="row">
+                            <div class="col-4">
+                                <img src="{{ asset('images/flag.svg') }}">
+                            </div>
+                            <div class="col-4">
+                                <h5>العربية</h5>
+
+
+                            </div>
+
+
+
+                        </div>
+
+                    </div>
+
+
+                    <div class="noti text-center">
+                        <span><i class="far fa-bell"></i></span>
+                    </div>
+
+
+
+                    <div class="search">
+
+                        <input type="text" name="search">
+                        <span class="srch"><i class="fas fa-search"></i></span>
+
+                    </div>
+
+                    <div class="datee">
+                        <div class="row">
+                            <span><i class="far fa-calendar-alt"></i></span>
+                            <p>{{ Carbon\Carbon::now()->format('d-m-Y') }}</p>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+            <!--end heed-->
+
+
+            <!--start setting-->
+            <div class="setting all-products typs">
+                <div class="container">
+                    <div class="row def">
+
+                        <img src="images/all-products.svg">
+                        <h5>الطلاب</h5>
+
+
+
+                    </div>
+
+                    <div class="products-search typs1">
+
+
+                    </div>
+
+
+
+                    <div class="pt-5">
+                        {{-- <div class="row" id="category_id_basic">
+                            @include('dashboard.students.includes.__basic_filter_sections')
+                        </div> --}}
+
+
+                        <div class="row" id="category_id_college">
+                            @include('dashboard.students.includes.__college_filter_sections')
+                        </div>
+                        {{-- <div class="row">
+                            <div class="col-3 mx-auto">
+
+
+                                <span class="btn btn-primary" onclick="filterbasicstudents()">بحث</span>
+                            </div>
+                        </div> --}}
+                        <div class="row">
+                            <div class="table-responsive">
+
+                                {!! $dataTable->table(
+                                    [
+                                        'class' => 'table_expenses table_topic table table-striped table-bordered',
+                                    ],
+                                    true,
+                                ) !!}
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <!--end setting-->
+
+
+            <!--start foter-->
+            <div class="foter">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h5>Made With <img src="{{ asset('images/red.svg') }}"> By Crazy Idea </h5>
+                        <p>Think Out Of The Box</p>
+                    </div>
+                </div>
+            </div>
+            <!--end foter-->
+        </div>
+    </div>
+    <!--end page-body-->
+@endsection
+
+@section('scripts')
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+
+    {{ $dataTable->scripts() }}
+
+
+    <script>
+        function activeuser(id) {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.ajax({
+                type: "get",
+                url: `activeuser/${id}`,
+                contentType: "application/json; charset=utf-8",
+                dataType: "Json",
+                success: function(result) {
+                    if (result.status == 'deactive') {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'تم الغاء التفعيل ',
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                        $(`#btn${id}`).html('تفعيل');
+
+                    } else if (result.status == 'active') {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'تم التفعيل  ',
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                        $(`#btn${id}`).html('الغاء التفعيل');
+
+                    }
+
+                }
+
+            });
+        }
+
+        function deleteuser(sel) {
+            let id = sel;
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            Swal.fire({
+                title: 'هل انت متاكد',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $.ajax({
+
+                        url: `deleteuser/${id}`,
+                        //    contentType: "application/json; charset=utf-8",
+                        dataType: "Json",
+                        success: function(result) {
+                            if (result.status == true) {
+                                $(`#s${id}`).remove();
+                                Swal.fire(
+                                    'Deleted!',
+                                    'تم مسح المستخدم بنجاح',
+                                    'success'
+                                )
+                            }
+                        }
+
+                    });
+                }
+
+
+            })
+        }
+
+        function student_logout(id) {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.ajax({
+                type: "get",
+                url: `student_logout/${id}`,
+                contentType: "application/json; charset=utf-8",
+                dataType: "Json",
+                success: function(result) {
+                    if (result.status == true) {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'تم  تسجيل الخروج ',
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+
+
+                    }
+
+                }
+
+            });
+        }
+    </script>
+
+
+
+
+
+{{-- <script>
+    function filter_based_on_category_id_education() {
+
+        // // Get selected options
+        // var selected = $("#issue_type_id option:selected");
+
+        // var issue_types = selected.map(function() {
+        //     return $(this).val();
+        // }).get(); // Convert to an array
+
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+
+        $.ajax({
+            url: `{{ route('client.issues.index') }}`,
+            type: 'POST',
+            data: {
+                "word": $(".word").val(),
+                "session_date": session_date,
+                "issue_types": issue_types,
+                "issue_status": issue_status,
+            },
+
+
+        }).done(function(result) {
+            // $('.selectpicker').selectpicker('refresh');
+            $('.category_id_basic').html(result);
+        }).fail(function(result) {
+            $('.case_container').html(result);
+        });
+
+    }
+</script> --}}
+
+<script>
+    function filter_students() {
+        $('#dataTableBuilder').on('preXhr.dt', function(e, settings, data) {
+            //basic filters
+            data.stage_id = $("#stage").val();
+            data.years_id = $("#year").val();
+            data.type_id = $("#types").val();
+            // //college filters
+            data.university_id = $("#university").val();
+            data.college_id = $("#college").val();
+            data.division_id = $("#division").val();
+            data.section_id = $("#section").val();
+            data.type_college_id = $("#typescollege").val();
+        });
+        $('#dataTableBuilder').DataTable().ajax.reload();
+    }
+</script>
+
+
+
+
+@endsection
