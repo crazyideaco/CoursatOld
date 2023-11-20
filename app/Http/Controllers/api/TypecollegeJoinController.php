bcda37111ba29306059553d37f44fc96378ffe04
OmniaSwaidan <omniaahmed129999@gmail.com>
2023-11-20 18:32:34 +0200

    add

--- app/Http/Controllers/api/TypecollegeJoinController.php
+++ app/Http/Controllers/api/TypecollegeJoinController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\api;
+
 use Illuminate\Http\Request;
 use App\TypecollegeJoin;
 use App\TypesCollege;
@@ -9,55 +10,55 @@ use Validator;
 use App\Type;
 use App\TypeJoin;
 
-class TypecollegeJoinController extends Controller{
+class TypecollegeJoinController extends Controller
+{
 
-    public function index(Request $request){
-        if(auth()->user()->category_id == 1){
-            $validator = Validator::make($request->all(),[
-                "typecollege_id" => "required" 
-            ],[
+    public function index(Request $request)
+    {
+        if (auth()->user()->category_id == 1) {
+            $validator = Validator::make($request->all(), [
+                "typecollege_id" => "required"
+            ], [
                 "typecollege_id.required" => "حقل الكورس مطلوب"
             ]);
-            if($validator->passes()){
-                $type = Type::where("id",$request->typecollege_id)->first();
-                if(!$type){
-                  $msg = "لا يوجد كورس بهذا الاسم";
-                  return response()->json(['status' => false,"message" => $msg]); 
+            if ($validator->passes()) {
+                $type = Type::where("id", $request->typecollege_id)->first();
+                if (!$type) {
+                    $msg = "لا يوجد كورس بهذا الاسم";
+                    return response()->json(['status' => false, "message" => $msg]);
                 }
-             $join = new TypeJoin;
-             $join->student_id = auth()->id();
-             $join->type_id = $request->typecollege_id;
-             $join->save();
-             $msg = "تم اضافه طلب الانضمام بنجاح";
-             return response()->json(['status' => true,"message" => $msg]);
-      }   else{
-        $msg = $validator->messages()->first();
-        return response()->json(['status' => false,"message" => $msg]);
-      }
-    
-     } else if(auth()->user()->category_id == 2){
-          $validator = Validator::make($request->all(),[
-              "typecollege_id" => "required" 
-          ],[
-              "typecollege_id.required" => "حقل الكورس مطلوب"
-          ]);
-          if($validator->passes()){
-              $type = TypesCollege::where("id",$request->typecollege_id)->first();
-              if(!$type){
-                $msg = "لا يوجد كورس بهذا الاسم";
-                return response()->json(['status' => false,"message" => $msg]); 
-              }
-           $join = new TypecollegeJoin;
-           $join->student_id = auth()->id();
-           $join->typecollege_id = $request->typecollege_id;
-           $join->save();
-           $msg = "تم اضافه طلب الانضمام بنجاح";
-           return response()->json(['status' => true,"message" => $msg]);
-          }else{
-            $msg = $validator->messages()->first();
-            return response()->json(['status' => false,"message" => $msg]);
-          }
-         }
+                $join = new TypeJoin;
+                $join->student_id = auth()->id();
+                $join->type_id = $request->typecollege_id;
+                $join->save();
+                $msg = "تم اضافه طلب الانضمام بنجاح";
+                return response()->json(['status' => true, "message" => $msg]);
+            } else {
+                $msg = $validator->messages()->first();
+                return response()->json(['status' => false, "message" => $msg]);
+            }
+        } else if (auth()->user()->category_id == 2) {
+            $validator = Validator::make($request->all(), [
+                "typecollege_id" => "required"
+            ], [
+                "typecollege_id.required" => "حقل الكورس مطلوب"
+            ]);
+            if ($validator->passes()) {
+                $type = TypesCollege::where("id", $request->typecollege_id)->first();
+                if (!$type) {
+                    $msg = "لا يوجد كورس بهذا الاسم";
+                    return response()->json(['status' => false, "message" => $msg]);
+                }
+                $join = new TypecollegeJoin;
+                $join->student_id = auth()->id();
+                $join->typecollege_id = $request->typecollege_id;
+                $join->save();
+                $msg = "تم اضافه طلب الانضمام بنجاح";
+                return response()->json(['status' => true, "message" => $msg]);
+            } else {
+                $msg = $validator->messages()->first();
+                return response()->json(['status' => false, "message" => $msg]);
+            }
+        }
     }
-    
 }
