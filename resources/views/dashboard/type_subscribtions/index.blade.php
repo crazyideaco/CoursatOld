ac588988a8fbc2ae2cf7b2937a8aa480efd7a4c6
OmniaSwaidan <omniaahmed129999@gmail.com>
2023-11-21 16:25:31 +0200

    add

--- resources/views/dashboard/type_subscribtions/index.blade.php
+++ resources/views/dashboard/type_subscribtions/index.blade.php
@@ -8,7 +8,7 @@
 @endsection
 @section('content')
 	<!--begin::Card-->
-    <div class="container">
+    {{-- <div class="container">
 
     <div class="card card-custom gutter-b">
 
@@ -27,8 +27,148 @@
 
                      ],true) !!}
             <!--end: Datatable-->
+ --}}
+ <div class="page-body">
+    <div class="container">
+        <!--start heed-->
+        <div class="heed">
+
+            <div class="row">
+                <div class="profile">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="{{ asset('images/profile.svg') }}">
+                        </div>
+                        <div class="col-6">
+                            <h5>{{ auth()->user()->name }}</h5>
+                            <p>ادمن</p>
+
+                        </div>
 
 
+                    </div>
+                </div>
+                <div class="flag">
+
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="{{ asset('images/flag.svg') }}">
+                        </div>
+                        <div class="col-4">
+                            <h5>العربية</h5>
+
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+
+                <div class="noti text-center">
+                    <span><i class="far fa-bell"></i></span>
+                </div>
+
+
+
+                <div class="search">
+
+                    <input type="text" name="search">
+                    <span class="srch"><i class="fas fa-search"></i></span>
+
+                </div>
+
+                <div class="datee">
+                    <div class="row">
+                        <span><i class="far fa-calendar-alt"></i></span>
+                        <p>{{ Carbon\Carbon::now()->format('d-m-Y') }}</p>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+        </div>
+        <!--end heed-->
+
+
+        <!--start setting-->
+        <div class="setting all-products typs">
+            <div class="container">
+                {{-- <div class="row def">
+
+                    <img src="images/all-products.svg">
+                    <h5>الطلاب</h5>
+
+
+
+                </div> --}}
+
+                <div class="products-search typs1">
+
+
+                </div>
+
+
+
+                <div class="all-infor">
+                    <div class="row" id="category_id_basic">
+                        <h4 class="hederre">
+                            المرحله الأساسيه
+                        </h4>
+                        @include('dashboard.students.includes.__basic_filter_sections')
+                    </div>
+
+
+                    <div class="row" id="category_id_college">
+                        <h4 class="hederre">
+                            المرحله الجامعيه
+                        </h4>
+                        @include('dashboard.students.includes.__college_filter_sections')
+                    </div>
+                    {{-- <div class="row">
+                        <div class="col-3 mx-auto">
+
+
+                            <span class="btn btn-primary" onclick="filterbasicstudents()">بحث</span>
+                        </div>
+                    </div> --}}
+                    <div class="row">
+                        <div class="table-responsive">
+
+                            {!! $dataTable->table(
+                                [
+                                    'class' => 'table_expenses table_topic table table-striped table-bordered',
+                                ],
+                                true,
+                            ) !!}
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+        <!--end setting-->
+
+
+        <!--start foter-->
+        <div class="foter">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h5>Made With <img src="{{ asset('images/red.svg') }}"> By Crazy Idea </h5>
+                    <p>Think Out Of The Box</p>
+                </div>
+            </div>
+        </div>
+        <!--end foter-->
+    </div>
+</div>
     <!--end::Card-->
 @endsection
 @section('scripts')
