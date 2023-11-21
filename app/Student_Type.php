d4145a9d04361f2bbcf661cc809d91860e7348ff
OmniaSwaidan <omniaahmed129999@gmail.com>
2023-11-21 15:19:19 +0200

    add

--- app/Student_Type.php
+++ app/Student_Type.php
@@ -14,4 +14,19 @@ class Student_Type extends Model
     public function student(){
         return $this->belongsTo(User::class, 'student_id');
     }
+
+    protected $appends = ['type_format'];
+
+    public function getTypeFormatAttribute()
+    {
+        if ($this->type == 1) {
+            return 'شراء';
+        } elseif ($this->type == 2) {
+            return 'طلب انضمام';
+        }elseif ($this->type == 3) {
+            return 'scan Qrcode';
+        }elseif ($this->type == 4) {
+            return 'Dashboard';
+        }
+    }
 }
