5511f42e53b21b5a792dfe4069ae5d2ae1e701f6
OmniaSwaidan <omniaahmed129999@gmail.com>
2023-11-22 15:31:16 +0200

    add

--- app/Http/Controllers/api/PointController.php
+++ app/Http/Controllers/api/PointController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\PointRequest;
 use App\Traits\ApiTrait;
+use Validator;
 
 class PointController extends Controller
 {
