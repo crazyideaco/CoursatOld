0d610a564acc64a020b2ab75e4325e561b907f66
ahmed <ahmedwry588@gmail.com>
2023-11-25 17:13:11 +0200

    edit paymentway controller and blade

--- resources/views/dashboard/payment_wayes/create.blade.php
+++ resources/views/dashboard/payment_wayes/create.blade.php
@@ -99,7 +99,7 @@
                                 @if (auth()->user()->isAdmin === 'admin')
                                     <div class="form-group col-3">
                                         <label> رقم المركز</label>
-                                        <select class="form-control" name="center_id" required>
+                                        <select class="form-control" multiple name="center_id" required name="center_id[]">
                                             @foreach ($centers as $center)
                                                 <option value="{{ $center->id }}">{{ $center->name }}</option>
                                             @endforeach
@@ -112,7 +112,7 @@
 
                                 <div class="form-group col-3">
                                     <label>صورة الوسيلة </label>
-                                    <input type="file" class="form-control" placeholder="ادخل الصورة " name="image" >
+                                    <input type="file" class="form-control" placeholder="ادخل الصورة " name="image">
                                     @error('image')
                                         <div style="color:red;">{{ $message }} </div>
                                     @enderror
