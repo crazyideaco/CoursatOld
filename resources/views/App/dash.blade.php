6972ad7eed4acb4e6d320090e344d097ac4ac5d5
mohamednasra12 <mohamednasra638@gmail.com>
2023-11-26 11:02:04 +0200

    sidbar

--- resources/views/App/dash.blade.php
+++ resources/views/App/dash.blade.php
@@ -972,8 +972,8 @@
                                     <img src="{{ asset('images/qenoicon/users.svg') }}" id="img">
                                 </div>
                                 <div class="col-6">
-                                    <a href="#studentsYajra" data-toggle="collapse"> النقاط</a>
-                                    <div id="studentsYajra"
+                                    <a href="#studentsYajra-9" data-toggle="collapse"> النقاط</a>
+                                    <div id="studentsYajra-9"
                                         >
 
                                         <a href="{{ route('point_requests') }}"
