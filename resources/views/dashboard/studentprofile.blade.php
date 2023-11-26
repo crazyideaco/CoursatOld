d1a08cd33730d342b30fb002259fb57032ec4ffd
mohamednasra12 <mohamednasra638@gmail.com>
2023-11-26 12:20:44 +0200

    student

--- resources/views/dashboard/studentprofile.blade.php
+++ resources/views/dashboard/studentprofile.blade.php
@@ -372,19 +372,139 @@ table.table {
                           <div class="table_details">
                             <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                                 <li class="nav-item">
-                                  <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+                                  <a class="nav-link active" id="pills-coruses-tab" data-toggle="pill" href="#pills-coruses" role="tab" aria-controls="pills-coruses" aria-selected="true">كورسات</a>
                                 </li>
                                 <li class="nav-item">
-                                  <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</a>
+                                  <a class="nav-link" id="pills-exams-tab" data-toggle="pill" href="#pills-exams" role="tab" aria-controls="pills-exams" aria-selected="false">الامتحانات</a>
                                 </li>
                                 <li class="nav-item">
-                                  <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
+                                  <a class="nav-link" id="pills-History-tab" data-toggle="pill" href="#pills-History" role="tab" aria-controls="pills-History" aria-selected="false">History</a>
                                 </li>
                               </ul>
                               <div class="tab-content" id="pills-tabContent">
-                                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
-                                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
-                                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+                                <div class="tab-pane fade show active" id="pills-coruses" role="tabpanel" aria-labelledby="pills-coruses-tab">
+                                    <div class="header-table">
+                                        <h4>كورسات</h4>
+                                        <div class="form-group">
+                                          <input type="date" class="form-control">
+                                        </div>
+                                      </div>
+                                      <div class="table-responsive">
+                                        <div class="table_details">
+                                          <table class="table">
+                                            <thead>
+                                              <tr>
+                                                <th scope="col">ID</th>
+                                                <th scope="col">اسم الكورس</th>
+                                                <th scope="col">تاريخ اشتراك الكورس</th>
+                                                <th scope="col">Handle</th>
+                                              </tr>
+                                            </thead>
+                                            <tbody>
+                                              <tr>
+                                                <th scope="row">1564115</th>
+                                                <td>Mark</td>
+                                                <td>Otto</td>
+                                                <td>@mdo</td>
+                                              </tr>
+                                              <tr>
+                                                <th scope="row">2</th>
+                                                <td>Jacob</td>
+                                                <td>Thornton</td>
+                                                <td>@fat</td>
+                                              </tr>
+                                              <tr>
+                                                <th scope="row">3</th>
+                                                <td colspan="2">Larry the Bird</td>
+                                                <td>@twitter</td>
+                                              </tr>
+                                            </tbody>
+                                          </table>
+                                        </div>
+                                      </div>
+                                </div>
+                                <div class="tab-pane fade" id="pills-exams" role="tabpanel" aria-labelledby="pills-exams-tab">
+                                    <div class="header-table">
+                                        <h4>امتحانات</h4>
+                                        <div class="form-group">
+                                          <input type="date" class="form-control">
+                                        </div>
+                                      </div>
+                                      <div class="table-responsive">
+                                        <div class="table_details">
+                                          <table class="table">
+                                            <thead>
+                                              <tr>
+                                                <th scope="col">ID</th>
+                                                <th scope="col">اسم الامتحان</th>
+                                                <th scope="col">Last</th>
+                                                <th scope="col">Handle</th>
+                                              </tr>
+                                            </thead>
+                                            <tbody>
+                                              <tr>
+                                                <th scope="row">1</th>
+                                                <td>Mark</td>
+                                                <td>Otto</td>
+                                                <td>@mdo</td>
+                                              </tr>
+                                              <tr>
+                                                <th scope="row">2</th>
+                                                <td>Jacob</td>
+                                                <td>Thornton</td>
+                                                <td>@fat</td>
+                                              </tr>
+                                              <tr>
+                                                <th scope="row">3</th>
+                                                <td colspan="2">Larry the Bird</td>
+                                                <td>@twitter</td>
+                                              </tr>
+                                            </tbody>
+                                          </table>
+                                        </div>
+                                      </div>
+                                </div>
+                                <div class="tab-pane fade" id="pills-History" role="tabpanel" aria-labelledby="pills-History-tab">
+                                    <div class="header-table">
+                                        <h4>History</h4>
+                                        <div class="form-group">
+                                          <input type="date" class="form-control">
+                                        </div>
+                                      </div>
+                                      <div class="table-responsive">
+                                        <div class="table_details">
+                                          <table class="table">
+                                            <thead>
+                                              <tr>
+                                                <th scope="col">ID</th>
+                                                <th scope="col">First</th>
+                                                <th scope="col">Last</th>
+                                                <th scope="col">Handle</th>
+                                              </tr>
+                                            </thead>
+                                            <tbody>
+                                              <tr>
+                                                <th scope="row">1</th>
+                                                <td>Mark</td>
+                                                <td>Otto</td>
+                                                <td>@mdo</td>
+                                              </tr>
+                                              <tr>
+                                                <th scope="row">2</th>
+                                                <td>Jacob</td>
+                                                <td>Thornton</td>
+                                                <td>@fat</td>
+                                              </tr>
+                                              <tr>
+                                                <th scope="row">3</th>
+                                                <td colspan="2">Larry the Bird</td>
+                                                <td>@twitter</td>
+                                              </tr>
+                                            </tbody>
+                                          </table>
+                                        </div>
+                                      </div>
+                                </div>
                               </div>
                             </div>
                         {{-- <div class="row">
