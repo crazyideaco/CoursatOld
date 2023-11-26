9c44e59056003e401288fc4e2b862db94589835a
ahmed <ahmedwry588@gmail.com>
2023-11-26 11:04:54 +0200

    edit on fetchPaymentWay API

--- app/Http/Controllers/api/PaymentwayController.php
+++ app/Http/Controllers/api/PaymentwayController.php
@@ -6,14 +6,28 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\paymentwayResource;
 use App\Models\PaymentWay;
 use App\Traits\ApiTrait;
+use App\User;
 use Illuminate\Http\Request;
 
 class PaymentwayController extends Controller
 {
 
-use ApiTrait;
-    public function fetchPaymentWays () {
-        $payments= PaymentWay::get();
-        return  $this->dataResponse("payments collection",paymentwayResource::collection($payments));
+    use ApiTrait;
+    public function fetchPaymentWays()
+    {
+        $payments = [];
+        $authuser = auth()->user();
+        if (count($authuser->stdcenters) > 0) {
+            $centers = $authuser->stdcenters;
+            foreach ($centers as $key => $value) {
+                array_push($payments, $value->paymentways);
+                // $payments[] = $value->paymentway;
+            }
+            // $payments = $centersArray;
+        } else {
+            $payments = PaymentWay::get();
+        }
+
+        return  $this->dataResponse("payments collection", paymentwayResource::collection($payments));
     }
 }
