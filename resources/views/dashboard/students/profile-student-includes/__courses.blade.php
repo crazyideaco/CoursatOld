3c4febf08e812b4ab126323e97395232aac30370
mostafamhashem <mostafa.m.hashem.1@gmail.com>
2023-11-27 11:28:00 +0200

    student show courses pivot

--- resources/views/dashboard/students/profile-student-includes/__courses.blade.php
+++ resources/views/dashboard/students/profile-student-includes/__courses.blade.php
@@ -24,8 +24,8 @@
                     <tr>
                         <th scope="row">{{ $item->id }}</th>
                         <td>{{ $item->name_ar }}</td>
-                        <td>{{ $item->pivot()->created_at }}</td>
-                        <td>{{ $item->pivot()->type }}</td>
+                        <td>{{ $item->pivot->created_at }}</td>
+                        <td>{{ $item->pivot->type }}</td>
                         <td>{{ $item->center_id ? $item->center->name : '--' }}</td>
                         @if ($student->category_id == config('project_types.system_category_type.category_id_college'))
                             <td onclick="deleteuser_from_stutypescollege({{ $student->id }},{{ $item->id }})"
