31ab1abb4032a184e44375dfc207914ac91234ae
mostafamhashem <mostafa.m.hashem.1@gmail.com>
2023-11-28 15:29:08 +0200

    fetchPaymentWays

--- resources/views/dashboard/point_requests/image.blade.php
+++ resources/views/dashboard/point_requests/image.blade.php
@@ -3,5 +3,5 @@
 @endphp
 
 @if ($image != null)
-    <img src="{{ $point->image_link }}" style="width: 50px; height: 50px;">'
+    <img src="{{ $point->image_link }}" style="width: 50px; height: 50px;">
 @endif
