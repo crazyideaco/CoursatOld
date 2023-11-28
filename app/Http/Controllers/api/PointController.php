d9805759b0efc1632e36a45c53336ae932c9656c
mostafamhashem <mostafa.m.hashem.1@gmail.com>
2023-11-28 14:43:37 +0200

    PointController buy_points test

--- app/Http/Controllers/api/PointController.php
+++ app/Http/Controllers/api/PointController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PointRequestResource;
 use App\Models\PointRequest;
 use App\Traits\ApiTrait;
+use Illuminate\Support\Facades\DB;
 use Validator;
 
 class PointController extends Controller
@@ -24,37 +25,39 @@ class PointController extends Controller
 
     public function buy_points(Request $request)
     {
+        try {
 
-        $rules = [
-            "payment_method_id" => "required|exists:payment_ways,id",
-            "points" => "required",
-            "image" => "required",
-        ];
-        $validator = Validator::make($request->all(), $rules);
+            $rules = [
+                "payment_method_id" => "required|exists:payment_ways,id",
+                "points" => "required",
+                "image" => "required",
+            ];
+            $validator = Validator::make($request->all(), $rules);
 
-        if ($validator->fails()) {
+            if ($validator->fails()) {
 
-            return $this->getvalidationErrors($validator);
+                return $this->getvalidationErrors($validator);
+            }
+
+            DB::beginTransaction();
+            $data['user_id'] = auth()->id();
+            $data['payment_way_id'] = $request->payment_method_id;
+            $data['points'] = $request->points;
+            if ($request->image) {
+                $data['image'] = base64_encode(file_get_contents($request->image));
+                // $image = $request->image;
+                // $image->move('uploads' , time(). '.'.$image->getClientOriginalExtension());
+                // $data['image'] =time(). '.'.$image->getClientOriginalExtension();
+            }
+
+            PointRequest::create($data);
+            DB::commit();
+            $msg = "تم طلب " . $request->points . " نقاط";
+            return $this->successResponse($msg);
+            
+        } catch (\Throwable $th) {
+            return $this->errorResponse($th->getMessage());
         }
-
-        $data['user_id'] = auth()->id();
-        $data['payment_way_id'] = $request->payment_method_id;
-        $data['points'] = $request->points;
-        if($request->image)
-        {
-            $data['image'] = base64_encode(file_get_contents($request->image));
-            // $image = $request->image;
-            // $image->move('uploads' , time(). '.'.$image->getClientOriginalExtension());
-            // $data['image'] =time(). '.'.$image->getClientOriginalExtension();
-        }
-
-        PointRequest::create($data);
-        $msg = "تم طلب " . $request->points . " نقاط";
-        return $this->successResponse($msg);
-        // return response()->json([
-        //     "status" => true,
-        //     "message" => $msg,
-        // ]);
     }
 
     public function fetch_buy_points(Request $request)
@@ -63,9 +66,8 @@ class PointController extends Controller
 
         $points = PointRequest::whereUserId($user_id)->get();
 
-        $msg ="fetch_buy_points";
+        $msg = "fetch_buy_points";
         $data = PointRequestResource::collection($points);
         return $this->dataResponse($msg, $data, 200);
-
     }
 }
